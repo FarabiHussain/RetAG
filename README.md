7fdd04797b75196a024cd2720bcfaa2913fd53c5
Farabi Hussain <farabihussain8@gmail.com>
2024-03-03 16:58:08 -0600

    update readme and added flags to build tool

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # Ret.A.G
-`RET`ainer `A`greement `G`enerator provides a GUI for AMCAIM staff that allows easy creation of retainer agreements when working with clients 
+`RET`ainer `A`greement `G`enerator provides a GUI for AMCAIM staff that allows easy creation of retainer agreements when working with clients.
 
 ## Usage
 - download the latest build.
 - unzip the contents.
 - run the executable.
 
-# Build from source files
+## Build using script (recommended):
+### Run `build_app_main.py` with optional flags:
+- no flags will build the exe with the next minor build number.
+- `--selector` to show version selector in the terminal.
+- `--deps` to install dependencies.
 
-## Build using build tool (recommended):
-run `build_app.py` and follow the instructions on the screen
+## Build manually:
+1. run ```pip install CTkMessagebox pyinstaller customtkinter docx2pdf python-docx python-dateutil``` install dependencies:
+    - [CTkMessagebox](https://pypi.org/project/CTkMessagebox/)
+    - [pyinstaller](https://pypi.org/project/pyinstaller/)
+    - [customtkinter](https://pypi.org/project/customtkinter/)
+    - [docx2pdf](https://pypi.org/project/docx2pdf/)
+    - [python-docx](https://pypi.org/project/python-docx/)
+    - [python-dateutil](https://pypi.org/project/python-dateutil/)
 
-## Build mannually:
-install dependencies:
-- [CTkMessagebox](https://pypi.org/project/CTkMessagebox/)
-- [pyinstaller/](https://pypi.org/project/pyinstaller/)
-- [customtkinter](https://pypi.org/project/customtkinter/)
-- [docx2pdf](https://pypi.org/project/docx2pdf/)
-- [python-docx](https://pypi.org/project/python-docx/)
-- [python-dateutil](https://pypi.org/project/python-dateutil/)
-
-then run the command:
-```python -m PyInstaller main.py --onefile -w --icon=assets\logo.ico --name="RetAG"```
+2. run ```python -m PyInstaller main.py --onefile -w --icon=assets\logo.ico --name="RetAG"```
--- build_app_main.py
+++ build_app_main.py
@@ -14,47 +14,56 @@ latest_build = unformat(ver[0]) + "." + unformat(ver[1]) + "." + unformat(ver[2]
 ver[2] = "[" + str(int(unformat(ver[2])) + 1) + "]"
 
 # install dependencies needed to build the file
-if '-deps' in sys.argv:
+if '--deps' in sys.argv:
     install_dependencies()
 
-while True:
-    os.system('cls')
-
-    print(
-        ("Use 'a' and 'd' keys to navigate, 'w' and 's' to change version, 'e' to confirm, 'q' to quit.\n") +
-        ("Latest build found: v" + latest_build + "\n") +
-        ("New build version: v" + ver[0] + "." + ver[1] + "." + ver[2])
-    )
-
-    kb = msvcrt.getch()
-
-    # remove formatting from each element
+# skip the interface if not in args
+if '--selector' not in sys.argv:
     for i in range(len(ver)):
         ver[i] = unformat(ver[i])
 
-    try: 
-        kb = kb.decode(encoding='utf-8')
-    except: 
-        pass
+    set_version(ver)
+    build_exe(cwd, ver)
 
-    if kb == "a":
-        cur = (cur - 1) % 3
-    elif kb == "d":
-        cur = (cur + 1) % 3
-    elif kb == "w":
-        ver[cur] = str(int(ver[cur]) + 1)
-    elif kb == "s" and int(ver[cur]) > 0:
-        ver[cur] = str(int(ver[cur]) - 1)
-    elif kb == "e":
+else:
+    while True:
+        os.system('cls')
+
+        print(
+            ("Use 'a' and 'd' keys to navigate, 'w' and 's' to change version, 'e' to confirm, 'q' to quit.\n") +
+            ("Latest build found: v" + latest_build + "\n") +
+            ("New build version: v" + ver[0] + "." + ver[1] + "." + ver[2])
+        )
+
+        kb = msvcrt.getch()
+
+        # remove formatting from each element
         for i in range(len(ver)):
             ver[i] = unformat(ver[i])
-        set_version(ver)
-        build_exe(cwd, ver)
-        break
-    elif kb == "q":
-        break
 
-    # add square brackets to the currently selected element
-    ver[cur] = "[" + ver[cur].replace(" ", "") + "]"
+        try: 
+            kb = kb.decode(encoding='utf-8')
+        except: 
+            pass
+
+        if kb == "a":
+            cur = (cur - 1) % 3
+        elif kb == "d":
+            cur = (cur + 1) % 3
+        elif kb == "w":
+            ver[cur] = str(int(ver[cur]) + 1)
+        elif kb == "s" and int(ver[cur]) > 0:
+            ver[cur] = str(int(ver[cur]) - 1)
+        elif kb == "e":
+            for i in range(len(ver)):
+                ver[i] = unformat(ver[i])
+            set_version(ver)
+            build_exe(cwd, ver)
+            break
+        elif kb == "q":
+            break
+
+        # add square brackets to the currently selected element
+        ver[cur] = "[" + ver[cur].replace(" ", "") + "]"
 
 
